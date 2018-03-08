8dd66d835ee126cc8e2d44d6df14b197c11b9077
Christian Marquardt <christian@marquardt.sc>
2018-03-08 21:57:59 +0100

    Another update of the automatic versioning scheme.

--- mylibrary/_version.py
+++ mylibrary/_version.py
@@ -32,21 +32,19 @@ def get_version(version_file=STATIC_VERSION_FILE):
             version = get_version_from_git_archive(version_info)
         if not version:
             version = Version("unknown", None, None)
-        return semver_format(version)
+        return pep440_format(version)
     else:
         return version
 
 
-def semver_format(version_info):
+def pep440_format(version_info):
     release, dev, labels = version_info
 
     version_parts = [release]
     if dev:
-        if release.endswith('-dev'):
+        if release.endswith('-dev') or release.endswith('.dev'):
             version_parts.append(dev)
-        elif '-' in release:
-            version_parts.append('.dev{}'.format(dev))
-        else:
+        else:  # prefer PEP440 over stric adhesion to semver
             version_parts.append('.dev{}'.format(dev))
 
     if labels:
@@ -130,7 +128,7 @@ def get_version_from_git_archive(version_info):
         release, _ = sorted(version_tags)  # prefer e.g. "2.0" over "2.0rc1"
         return Version(release, dev=None, labels=None)
     else:
-        return Version('unknown', dev=None, labels=["{git_hash}".format(git_hash = git_hash)])
+        return Version('unknown', dev=None, labels=["g{}".format(git_hash)])
 
 
 __version__ = get_version()
