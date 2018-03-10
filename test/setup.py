bb3b974abef60b0e0daeff4e3226b0cec20ac81a
Christian Marquardt <christian@marquardt.sc>
2018-03-11 00:06:49 +0100

    Updated setup.py in test subdirectory.
    Also added setup.cfg files supporting pytest.

--- test/setup.py
+++ test/setup.py
@@ -15,128 +15,45 @@ Usage:
     python setup.py build_ext --inplace
 """
 
+#-------------------------------------------------------------------------------
+# Imports
+#-------------------------------------------------------------------------------
+
 from __future__ import division, print_function, absolute_import
 
-try:
-    # Python 3
-    MyFileNotFoundError = FileNotFoundError
-except:  # FileNotFoundError does not exist in Python 2.7
-    # Python 2.7
-    # - open() raises IOError
-    # - remove() (not currently used here) raises OSError
-    MyFileNotFoundError = (IOError, OSError)
-
-#########################################################
-# General config
-#########################################################
-
-
-#########################################################
-# Init
-#########################################################
-
-# check for Python 2.7 or later
-# http://stackoverflow.com/questions/19534896/enforcing-python-version-in-setup-py
+import os
 import sys
+
+from setuptools           import setup
+from setuptools           import Command
+from setuptools.extension import Extension
+
 if sys.version_info < (2,7):
     sys.exit('Sorry, Python < 2.7 is not supported')
 
-import os
 
-from setuptools import setup
-from setuptools.extension import Extension
+#-------------------------------------------------------------------------------
+# Configuration options
+#-------------------------------------------------------------------------------
 
-# Cython installed? Must be imported after setuptools Extension...
-# ...and we insist on being able to load because we're testing some
-# Cython functionality.
+# Note: By default, the same compiler and linker options are used as for building
+# Python. In this section, *additional* options can be specified.
 #
-from Cython.Build import cythonize
+# In addition, absolute cimports in Cython require to include "." in include_dirs
+# (see https://github.com/cython/cython/wiki/PackageHierarchy)
+
+include_dirs = ["."]
+
+# Additional libraries; always include libmath
+
+libraries = ["m"]
 
 
-#########################################################
-# Definitions
-#########################################################
-
-# Define our base set of compiler and linker flags.
-#
-# This is geared toward x86_64, see
-#    https://gcc.gnu.org/onlinedocs/gcc-4.6.4/gcc/i386-and-x86_002d64-Options.html
-#
-# Customize these as needed.
-#
-# Note that -O3 may sometimes cause mysterious problems, so we limit ourselves to -O2.
-
-extra_compile_args = ['-O2']
-extra_compile_args = ['-O0', '-g']
-extra_link_args    = []
-extra_link_args    = []
-
-# Additional flags to compile/link with OpenMP
-#
-openmp_compile_args = ['-fopenmp']
-openmp_link_args    = ['-fopenmp']
-
-
-#########################################################
+#-------------------------------------------------------------------------------
 # Helpers
-#########################################################
-
-# Make absolute cimports work.
-#
-# See
-#     https://github.com/cython/cython/wiki/PackageHierarchy
-#
-my_include_dirs = ["."]
-
-
-# Wrapper to create (Cython-based) extensions
-#
-# FIXME: Add libraries - also from a setup.cfg file?
-# FIXME: Add arbitrary source code / C files in addition to the main cython .pyx
-def declare_extension(extName, openmp = False, include_dirs = None):
-    """Declare a Cython extension module for setuptools.
-
-Parameters:
-    extName : str
-        Absolute module name, e.g. use `mylibrary.mypackage.mymodule`
-        for the Cython source file `mylibrary/mypackage/mymodule.pyx`.
-
-    openmp : bool
-        If True, compile and link with OpenMP.
-
-Return value:
-    Extension object
-        that can be passed to ``setuptools.setup``.
-"""
-    extPath = extName.replace(".", os.path.sep) + ".pyx"
-
-    compile_args = list(extra_compile_args) # copy
-    link_args    = list(extra_link_args)
-    libraries    = ["m"]  # link libm; this is a list of library names without the "lib" prefix
-
-    # OpenMP
-    if openmp:
-        compile_args.insert( 0, openmp_compile_args )
-        link_args.insert( 0, openmp_link_args )
-
-    # See
-    #    http://docs.cython.org/src/tutorial/external.html
-    #
-    # on linking libraries to your Cython extensions.
-    #
-    return Extension( extName,
-                      [extPath],
-                      extra_compile_args = compile_args,
-                      extra_link_args    = link_args,
-                      include_dirs       = include_dirs,
-                      libraries          = libraries
-                    )
-
+#-------------------------------------------------------------------------------
 
 # Clean command (from https://stackoverflow.com/a/1712544)
-#
-from setuptools import Command # or distutils.core?
-import os, sys
 
 class CleanCommand(Command):
     description = "clean after a build, forcefully removing dist/build and .egg-info directories"
@@ -147,27 +64,44 @@ class CleanCommand(Command):
         self.cwd = os.getcwd()
     def run(self):
         assert os.getcwd() == self.cwd, "Must be in package root: %s" % self.cwd
-        os.system("rm -rfv ./build ./dist ./*.egg-info ./__pycache__ *.so *.c")
+        os.system("rm -rfv ./build ./dist ./*.egg-info ./.pytest_cache ./__pycache__ *.so")
 
 
-cmdclass = {"clean": CleanCommand}
+# Cythonise command
+
+class CythonizeCommand(Command):
+    description  = "run cython on the cython-based extensions"
+    user_options = []
+    ext_modules  = None
+    def initialize_options(self):
+        self.cwd = None
+    def finalize_options(self):
+        self.cwd = os.getcwd()
+    def run(self):
+        from Cython.Build import cythonize
+        if self.ext_modules is not None:
+            dummy = cythonize(self.ext_modules)
+        else:
+            print("WARNING: Nothing found to cythonize...")
+
+cmdclass = {"clean": CleanCommand,
+            "cython": CythonizeCommand,
+            "cythonise": CythonizeCommand,
+            "cythonize": CythonizeCommand
+           }
 
 
-#########################################################
-# Set up modules
-#########################################################
+#-------------------------------------------------------------------------------
+# Set up extension / cython modules
+#-------------------------------------------------------------------------------
 
 # Declare Cython extension modules
-#
-ext_module_cythonmodule = declare_extension("cython_module", openmp = False)
 
-# This is mainly to allow a manual logical ordering of the declared modules
-#
-ext_modules = [ext_module_cythonmodule]
-
-# ...and cythonize (if needed)
-#
-ext_modules = cythonize(ext_modules)
+ext_modules = [Extension("cython_module",
+                         ["cython_module.pyx"],
+                         include_dirs = include_dirs,
+                         libraries    = libraries)
+              ]
 
 
 #########################################################
@@ -175,17 +109,16 @@ ext_modules = cythonize(ext_modules)
 #########################################################
 
 setup(
-    # See
-    #    http://setuptools.readthedocs.io/en/latest/setuptools.html
-    #
-    setup_requires = ["cython", "numpy"],
+    # See http://setuptools.readthedocs.io/en/latest/setuptools.html
+
+    setup_requires   = ["setuptools>=18.0", "numpy", "pytest-runner"],
     install_requires = ["numpy"],
 
     # All extension modules (list of Extension objects)
-    #
+
     ext_modules = ext_modules,
 
     # Custom commands
-    #
+
     cmdclass = cmdclass
 )
